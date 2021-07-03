e261e987c36031fe35b9d2b2c752570bb7993943
SanthosiKondupalli <santhosi.kondupalli@gmail.com>
2021-07-02 19:16:11 -0700

    Added Expense class

---
+++ ExpenseTracker/ExpenseTracker/ExpenseTracker/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExpenseTracker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker/ExpenseTracker/Model/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Model
+{
+    public class Expense
+    {
+        public String Name { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date{ get; set; }
+        public String Category { get; set; }
+
+
+
+
+    }
+}
