a71a9acb8fe754abc0f94edc60f8dcb18673670a
SanthosiKondupalli <santhosi.kondupalli@gmail.com>
2021-07-06 17:34:39 -0700

    Home page View
    Have added few main page view components by created some objects directly in the code

--- ExpenseTracker/ExpenseTracker/ExpenseTracker/MainPage.xaml.cs
+++ ExpenseTracker/ExpenseTracker/ExpenseTracker/MainPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ExpenseTracker.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +12,34 @@ namespace ExpenseTracker
 {
     public partial class MainPage : ContentPage
     {
+        String budgetFileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "MonthlyBudget.txt");
         public MainPage()
         {
             InitializeComponent();
+            if (File.Exists(budgetFileName))
+            {
+                Budget.Text = File.ReadAllText(budgetFileName);
+                SaveButton.IsVisible = false;
+                Budget.IsReadOnly = true;
+               
+                
+            }
+        }
+        protected override void OnAppearing()
+        {
+            MyExpensesList.ItemsSource=ExpenseManager.GetExpenses();
         }
 
         private void OnSaveButtonClicked(object sender, EventArgs e)
         {
+
+            if (!File.Exists(budgetFileName))
+            {
+                File.WriteAllText(budgetFileName, Budget.Text);
+                Budget.Text = File.ReadAllText(budgetFileName);
+            }
+            else
+                Budget.Text = File.ReadAllText(budgetFileName);
 
         }
     }
---
+++ ExpenseTracker/ExpenseTracker/ExpenseTracker/Model/ExpenseManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Model
+{
+    public static class ExpenseManager
+    {
+        public static List<Expense> GetExpenses()
+        {
+            List<Expense> expenses = new List<Expense>();
+
+            expenses.Add(new Expense {Name="Fruits",Amount=10, Date=DateTime.Now, Category="Food" });
+            expenses.Add(new Expense { Name = "Books", Amount = 30, Date = DateTime.Now, Category = "Misc" });
+            return expenses;
+        }
+    }
+}
