b8a123f6a39c2c7f53445454d805f183b7394424
Mark Grybos <markgrybos@gmail.com>
2021-07-09 16:29:27 -0700

    Added Expense Type Icons

---
+++ ExpenseTracker/ExpenseTracker/ExpenseTracker/Model/ExpenseIcon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Model
+{
+    public class ExpenseIcon
+    {
+        public string IconFile { get; set; }
+        public string IconName { get; set; }
+    }
+}
